f68e04bdb5e46926c12edea7379d4b113f626bff
Abhigyan Roy <163134341+abhigyan-10@users.noreply.github.com>
2026-06-23 16:31:55 +0530

    update p1.c

--- buttons/p1.c
+++ buttons/p1.c
@@ -24,3 +24,32 @@ void main()
 		}
     }
 }
+
+
+#include <REGX52.H>
+
+sbit SW1 = P0^0;
+sbit LED1 = P1^0;
+void delay()
+{
+	volatile unsigned int i,j;
+
+    for(i=0;i<20;i++)
+        for(j=0;j<1275;j++);
+}
+void main()
+{
+		P1 = 0x00;
+		while(1)
+    {
+			if(SW1 == 0)
+			{
+				delay();
+				if(SW1 == 0){
+					P1 = ~P1;
+					while(SW1 == 0);
+					delay();
+				}
+			}
+   }
+}
